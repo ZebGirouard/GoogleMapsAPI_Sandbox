ae6901716c6a9894416ae835d7ab64d44af85aa0
Timothy Zeb Girouard <ZebGirouard@users.noreply.github.com>
2016-03-20 13:48:23 -0600

    Update mapLoad.js

--- assets/js/mapLoad.js
+++ assets/js/mapLoad.js
@@ -2,12 +2,14 @@
       var mapKML;
       function initMap() {
         //GeoJSON Map
+        var mapOptions = {
+          zoom: 2,
+          center: {lat: -33.865427, lng: 151.196123},
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
         var mapDiv = document.getElementById('map');
-        map = new google.maps.Map(mapDiv, {
-          center: {lat: 44.540, lng: -78.546},
-          zoom: 8
-        });
-        //map.data.loadGeoJson('assets/usStates.json');
+        map = new google.maps.Map(mapDiv, mapOptions);
+        //map.data.loadGeoJson('assets/usStates.json'); //Input GeoJson file data
         var script = document.createElement('script');
         script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
         document.getElementsByTagName('head')[0].appendChild(script);
